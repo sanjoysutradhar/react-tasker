03fe5ef8d6d034b311c807cee8136cd790f81d78
Shanjoy Sutradhar <shanjoy.swe@gmail.com>
2024-11-18 10:35:26 +0600

    Header logo name change

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,10 @@
-import lwsLogo from "./assets/lws-logo-en.svg";
 export default function Header() {
   return (
     <nav className="py-6 md:py-8 fixed top-0 w-full !bg-[#191D26] z-50">
       <div className="container mx-auto flex items-center justify-between gap-x-6">
         <a href="/">
-          <img className="h-[45px]" src={lwsLogo} alt="Lws" />
+          {/* <img className="h-[45px]" src={lwsLogo} alt="Lws" /> */}
+          <span className="text-2xl text-teal-700">Learn With Shanjoy </span>
         </a>
       </div>
     </nav>
